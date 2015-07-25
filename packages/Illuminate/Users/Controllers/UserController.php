a24dcbf931dad454f35466c9ad36423e67019d0d
huytbt <huytbt@gmail.com>
2015-07-25 23:47:32 +0700

    Register action

--- packages/Illuminate/Users/Controllers/UserController.php
+++ packages/Illuminate/Users/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace PhpSoft\Illuminate\Users\Controllers;
 
 use Auth;
 use JWTAuth;
+use Validator;
+use Illuminate\Http\Request;
+use PhpSoft\Illuminate\Users\Models\User;
 
 class UserController extends Controller
 {
@@ -16,7 +19,7 @@ class UserController extends Controller
     {
         parent::__construct();
 
-        $this->middleware('jwt.auth');
+        $this->middleware('jwt.auth', ['except' => 'register']);
     }
 
     /**
@@ -30,5 +33,32 @@ class UserController extends Controller
         return response()->json(arrayView('user/read', [
             'user' => Auth::user()
         ]), 200);
+    }
+
+    /**
+     * Register action
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required|max:255',
+            'email'     => 'required|email|max:255|unique:users',
+            'password'  => 'required|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(arrayView('errors/validation', [
+                'errors' => $validator->errors()
+            ]), 400);
+        }
+
+        $user = User::create($request->all());
+
+        return response()->json(arrayView('user/read', [
+            'user' => $user
+        ]), 201);
     }
 }
