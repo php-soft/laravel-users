91c6eb85cbe833a6d71455df7b8a04516633288e
huytbt <huytbt@gmail.com>
2015-08-03 18:58:29 +0700

    Change action POST /register to POST /users

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::post('/auth/login', '\PhpSoft\Illuminate\Users\Controllers\AuthController@login');
-Route::post('/register', '\PhpSoft\Illuminate\Users\Controllers\UserController@register');
+Route::post('/users', '\PhpSoft\Illuminate\Users\Controllers\UserController@create');
 Route::group(['middleware'=>'jwt.auth'], function() { 
     Route::post('/auth/logout', '\PhpSoft\Illuminate\Users\Controllers\AuthController@logout');
     Route::get('/me', '\PhpSoft\Illuminate\Users\Controllers\UserController@authenticated');
