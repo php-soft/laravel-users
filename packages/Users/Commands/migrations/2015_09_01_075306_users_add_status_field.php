2f68c10d4aa7a356f4f9f19a7034e5420b65bc4e
gghoantt <hoantt@greenglobal.vn>
2015-09-03 14:33:24 +0700

    Create migrations file on Users package #47

---
+++ packages/Users/Commands/migrations/2015_09_01_075306_users_add_status_field.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace {
+
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
+
+    class UsersAddStatusField extends Migration
+    {
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::table('users', function (Blueprint $table) {
+                $table->integer('status')->default(0);
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+
+        }
+    }
+}
