a36ccd7b2bd4d7ad49c87eaef83bdeef02aaa6ff
Hoa Nguyen <hoantt@greenglobal.vn>
2015-11-16 14:00:33 +0700

    Add file config/phpsoft.user.php #86

---
+++ config/phpsoft.user.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Model namespace
+    |--------------------------------------------------------------------------
+    |
+    | Specify the full namespace to your User model.
+    | e.g. 'Acme\Entities\User'
+    |
+    */
+
+    'model' => 'App\User',
+
+];
